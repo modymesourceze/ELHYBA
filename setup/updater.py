a08416c1f569b5a117cd6d7ab2c5aadeaa87aca5
Mody <148470115+modymesourceze@users.noreply.github.com>
2025-01-02 16:26:45 +0200

    Add files via upload

--- setup/updater.py
+++ setup/updater.py
@@ -42,9 +42,9 @@ async def update_requirements(main , test):
     try:
         for i in a:
             await runcmd(f"pip install {i}")
-            print(f"تم بنجاح تحميل : {i}")
+            print(f"Succesfully installed {i}")
     except Exception as e:
-        print(f"هناك خطا اثناء تنزيل المكاتب {str(e)}")
+        print(f"Error while installing requirments {str(e)}")
 
 
 loop = asyncio.get_event_loop()
